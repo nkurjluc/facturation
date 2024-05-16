96b3ff8ef972d9851e53b47ab897448fa4b7855f
nkurjluc <nkurjluc@gmail.com>
2024-05-16 12:18:45 -0400

    My first commit

---
+++ src/test/java/billingSystem/facturation/FacturationApplicationTests.java
@@ -0,0 +1,13 @@
+package billingSystem.facturation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacturationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
