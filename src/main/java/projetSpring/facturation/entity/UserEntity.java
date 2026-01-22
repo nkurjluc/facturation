ad84bd6d9a7922002617673e21059b242287c65b
nkurjluc <nkurjluc@gmail.com>
2026-01-22 13:23:54 -0500

    Commit du 22.01.2026

---
+++ src/main/java/projetSpring/facturation/entity/UserEntity.java
@@ -0,0 +1,10 @@
+package projetSpring.facturation.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tbl_utilisateur")
+public class UserEntity {
+    
+}
